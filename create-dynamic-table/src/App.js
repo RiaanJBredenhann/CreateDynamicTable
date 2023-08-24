c209c9995e5eafcae8442aafd112eb01fb239da0
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-24 16:28:22 +0200

    update

--- create-dynamic-table/src/App.js
+++ create-dynamic-table/src/App.js
@@ -25,5 +25,14 @@
 // export default App;
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic-table/DynamicTable";
 
+function App() {
+  render() {
+    return (
+      <DynamicTable />
+    );
+  }
+}
 
+export default App;
